5f83575bbd7b4c0e17c78ffd36e86ebfa92ab9a4
git-grab-scrap <118714201+git-grab-scrap@users.noreply.github.com>
2023-07-25 22:44:15 +0530

    day-3 exercise

---
+++ day-3.py
@@ -0,0 +1,6 @@
+user_num1, user_num2 = input("Enter two numbers of your choice for computational tasks: ").split()
+add = int(user_num1) + int(user_num2)
+sub = add = int(user_num2) - int(user_num1)
+mul = add = int(user_num1) * int(user_num2)
+divi = add = int(user_num1) // int(user_num2)
+print(f" Computational Results for user choice of numbers==== \n Addition: {add} \n Subtraction: {sub} \n Multiplication: {mul} \n Division: {divi}  ")
