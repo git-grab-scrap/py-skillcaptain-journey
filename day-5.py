abe96fa215830b43603bdc98f921a86f011c4020
git-grab-scrap <118714201+git-grab-scrap@users.noreply.github.com>
2023-07-27 22:17:44 +0530

    day-5 exercise

---
+++ day-5.py
@@ -0,0 +1,9 @@
+print("Displaying the fisr 10 numbers: \n")
+num = 1
+while num < 10:
+  print(num)
+  num += 1
+name_list = ['John','Raju','Madhu','Xin','Yongmin','Dave']
+for name in name_list:
+  print(f"Hello there. Greetings of the day to {name}.")
+print("That's it for loops!!")
